373f4ae73aa966673a6e32d38cce809edfb702da
PREVIN98 <108812056+PREVIN98@users.noreply.github.com>
2022-07-24 20:28:05 +0530

    modify name

--- help.java
+++ help.java
@@ -3,7 +3,7 @@ package general;
 public class help {
 
 	public static void main(String[] args) {
-		System.out.println("HELLO WORLD");
+		System.out.println("HELLO sabri");
 		System.out.println(" I AM PROGRAMMER");
 
 ;		// TODO Auto-generated method stub
