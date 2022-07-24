d6a9389b4e6bf559d5ba2a673c307eca9d5f8fd1
previn <previnseven598@gmail.com>
2022-07-24 20:12:58 +0530

    first commit

---
+++ help.java
@@ -0,0 +1,13 @@
+package general;
+
+public class help {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO WORLD");
+		System.out.println(" I AM PROGRAMMER");
+
+;		// TODO Auto-generated method stub
+
+	}
+
+}
